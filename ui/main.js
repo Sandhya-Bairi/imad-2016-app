490adf0e8968bcef3151ee8a630997e8face90a3
Sandhya-Bairi <sandhya.bairi154@gmail.com>
2016-09-21 07:57:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,11 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='New Value';
+
+//Move the image
+
+var img=document.getElementById('madi');
+
+img.onClick=function(){
+    img.style.marginLeft='100px';
+};
