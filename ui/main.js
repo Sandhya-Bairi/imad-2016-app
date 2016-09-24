05e61093640964c7506484f301c4ef448618c180
Sandhya-Bairi <sandhya.bairi154@gmail.com>
2016-09-24 07:36:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,17 +51,31 @@ var name=nameInput.value;
 var submit= document.getElementById("submit_btn");
 submit.onclick = function(){
 //Make a request to the server and send the name
-
+ var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                var names= request.responseText;
+                names=JSON.parse(names);  //converting string to object in this case an array
+                var list="";
+                for (var i=0;i<names.length;i++)
+                {
+                    list+= "<li>"+names[i]+"</li>";
+                }
+                var ul=document.getElementById('nameList');
+                ul.innerHTML=list;
+            }
+        }
+        //not done yet
+    };
+    //Make the request
+    request.open('GET','http://sandhya-bairi.imad.hasura-app.io/submit-name?name='+name ,true);
+    request.send(null);
 // capture the list of names and render it as a list
     
-    var names=["name1","name2","name3","name4"];
-    var list="";
-    for (var i=0;i<names.length;i++)
-    {
-        list+= "<li>"+names[i]+"</li>";
-    }
-    var ul=document.getElementById('nameList');
-    ul.innerHTML=list;
+   
 
 };
 
